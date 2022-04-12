0e46c97b68fdebc6022b9865a5e8952a6374cef9
myrol <posthomerik@gmail.com>
2022-04-12 22:08:05 +0200

    Hashmap Angelegt Erste Schritte mit isWellSorted()

--- Sequenz.java
+++ Sequenz.java
@@ -1,19 +1,36 @@
+import org.junit.jupiter.api.BeforeAll;
 import java.util.HashMap;
-
 public class Sequenz implements SequenzIF {
     String[][] abh;
-
     public Sequenz(String[][] _abh) {
         abh = _abh;
     }
 
     @Override
-    public boolean isWellSorted(String[] arr) {
+    public boolean isWellSorted(String[] seq) {
+        HashMap<String, Boolean> h = fill(seq);
+
+        // Schleife fuer die Sequenz
+        for (int i = 0; i < seq.length; i++) {
+            String currentLetter = seq[i];
+            // Pruefe, ob aktueller Buchstabe ein Nachfolger ist
+            for(int j = 0; j < abh.length; j++) {
+                if (abh[j][1].equals(currentLetter) && h.get(abh[j][0]) == false) {
+                    return false;
+                }
+            }
+        }
         return false;
     }
 
     @Override
     public HashMap<String, Boolean> fill(String[] arr) {
-        return null;
+        HashMap<String, Boolean> h = new HashMap<String, Boolean>();
+
+        for (int i = 0; i < arr.length; i++) {
+            h.put(arr[i], false);
+        }
+
+        return h;
     }
 }
--- SequenzIF.java
+++ SequenzIF.java
@@ -2,5 +2,5 @@ import java.util.HashMap;
 
 public interface SequenzIF {
     boolean isWellSorted(String[] arr);
-    HashMap<String,Boolean> fill(String[] arr);
+    HashMap<String, Boolean> fill(String[] arr);
 }
