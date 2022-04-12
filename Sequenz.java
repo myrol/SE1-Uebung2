a755c6ad858519dfe0a4cf2792dc76dc750e6d98
Tony <tonyredgravedmcv@gmail.com>
2022-04-12 21:31:51 +0200

    Implementiert eine Klasse Sequenz, sowie UnitTests basierend auf dem Interface SequenzIF

--- Sequenz.java
+++ Sequenz.java
@@ -1,7 +1,19 @@
-public class Sequenz {
+import java.util.HashMap;
+
+public class Sequenz implements SequenzIF {
     String[] seq;
 
     public Sequenz(String[] _seq) {
         seq = _seq;
+    }
+
+    @Override
+    public boolean isWellSorted(String[] arr) {
+        return false;
+    }
+
+    @Override
+    public HashMap<String, boolean> fill(String[] arr) {
+        return null;
     }
 }
---
+++ SequenzIFTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SequenzIFTest {
+
+    @Test
+    void isWellSorted() {
+    }
+
+    @Test
+    void fill() {
+    }
+}
