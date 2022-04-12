07ae9f3364b5841411c9d14b1b25c19e3a00d172
xEspeox <103063971+xEspeox@users.noreply.github.com>
2022-04-12 23:26:34 +0200

    JUnit Tests

--- SequenzTest.java
+++ SequenzTest.java
@@ -1,5 +1,21 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class SequenzTest {
+public class SequenzTest {
+    String[] sequence = {"A","B","C","D"};
+    Testing test;
+    @BeforeEach
+    void TestingTestSetup(){
+        test = new Testing(new String[][]{{"A", "C"}, {"C", "D"}, {"B", "C"}});
+    }
 
+    @Test
+    @DisplayName("Die Methode wird auf ihre Ausgabe getestet.")
+    void TestisWellSorted(){
+        assertTrue(test.isWellSorted(sequence));
+        assertFalse(test.isWellSorted(new String[]{"D","C","B","A"}));
+    }
 }
