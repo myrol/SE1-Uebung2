6a49af3dc1f6ae50cd6e7cf02a05aaa92656d132
TKaysers <tkmail96@gmx.de>
2022-04-12 23:32:57 +0200

    Sequenzmethoden zur Erfuellung der JUnit-Tests

--- Sequenz.java
+++ Sequenz.java
@@ -1,9 +1,36 @@
-public class Sequenz {
-    String[] seq;
+public class Sequenz implements SequenzIF{
+    String[][] seq;
 
+    public Sequenz(String[][]seq){
+        this.seq = seq;
+    }
 
-    //Test fuer master2
-    public Sequenz(String[] _seq) {
-        seq = _seq;
+    public boolean isWellSorted(String[] sequence){
+        int counter = 0;
+
+        for(int k=0; k<sequence.length; k++){
+            for(int i=seq[0].length-1;i>=0; i--) {
+                for (int j = 0; j < seq.length; j++) {
+                    if (i > 0) {
+                        if (seq[j][i].equals(sequence[k]) && seq[j][i-1].equals("ok")) { //"A,B,C"
+                            seq[j][i] = "ok";
+                            counter++;
+                        }else if (seq[j][i].equals(sequence[k])){
+                            return false;
+                        }
+                    } else {
+                        if (seq[j][i].equals(sequence[k])) {
+                            seq[j][i] = "ok";
+                            counter++;
+                        }
+                    }
+                }
+            }
+        }
+        if(seq[0].length * seq.length > counter){
+            return false;
+        }else{
+            return true;
+        }
     }
 }
---
+++ SequenzIF.java
@@ -0,0 +1,3 @@
+public interface SequenzIF {
+    boolean isWellSorted(String[] sequence);
+}
